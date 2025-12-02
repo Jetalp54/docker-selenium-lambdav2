bedd8bdb6ac8f457c7b5e6b0689a3178e42088a4
Jetalp54 <160750895+Jetalp54@users.noreply.github.com>
2025-12-02 17:11:06 +0100

    Update prep_handler.py

--- prep_handler.py
+++ prep_handler.py
@@ -17,8 +17,12 @@ import traceback
 import boto3
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 
 # Modern User-Agents for rotation
 USER_AGENTS = [
